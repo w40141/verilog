e3fa78fdbac5aa07e5cd2cc6973006ccf525c91b
daisuke oku <w.40141@gmail.com>
2015-08-27 15:16:05 +0900

    remove work

--- work/and_2.v
+++
@@ -1,6 +0,0 @@
-// and_2
-module and_2 (IN1, IN2, OUT);
-    input IN1, IN2;
-    output OUT;
-    and U1 (OUT, IN1, IN2);
-endmodule
--- work/exor.v
+++
@@ -1,5 +0,0 @@
-module EXOR(IN1, IN2, OUT);
-    input   IN1, IN2;
-    output  OUT;
-    assign  OUT = IN1 ^ IN2;
-endmodule
