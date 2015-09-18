9df9a2d5f49b206be0573c52c636f8b9e21c5590
w40141 <w.40141@gmail.com>
2015-09-18 19:28:48 +0900

    test

--- Trivium/work/test_te.v
+++ Trivium/work/test_te.v
@@ -14,29 +14,6 @@ module test;
     // ENCRIPT ENCRIPT (KEY, IV, clk, reset, OUT);
 
     initial begin
-// Set 2, vector# 16:
-//                          key = 10101010101010101010
-//                           IV = 00000000000000000000
-//                stream[0..63] = 4567A95BDBA94E17A52438D060DC8EF8
-//                                20A7DA9E51E6F1D67247D3807F41338E
-//                                3F8DE39BEF9E04BB9EE9F952B411A007
-//                                D2F0E295B255A831C264C47D1C078A2B
-//             stream[192..255] = F5393FE158303A91214E77B9A76FE062
-//                                FD25B7F186137F0D5673B3E3B53CA3CF
-//                                2F8B823820EA9646B77D4DBC4A22EF52
-//                                D1498698229EAFD28EA151BF581481B6
-//             stream[256..319] = C934D399C84DD90EBB1274208437C97C
-//                                BDB3C43F65469D131BAD9B49800A9186
-//                                EC066A6291706A792398EACF43D97F94
-//                                3033B0A1FF96CE8448E37260D740D36D
-//             stream[448..511] = EF7226C691BC2EE81DC34FBEAB365944
-//                                4A7C976A4894CCFC6D558E73B2FBF33A
-//                                1DB9FAAB1D11DB6293D7AC126399057D
-//                                C1711DD057FE13395F69D69D37614134
-//                   xor-digest = AB585980991C6ACD1889A72ECF42D41E
-//                                6BB1EC325FFAD62E7A8514AA87ADAB84
-//                                873B44D96413F3AF9AE1A93C02F3E591
-//                                E2C4320B3BEA6BE96B1A9337E81AF2B7
         clk = 1'b0;
         // KEY = 80'h80000000000000000000;
         // IV  = 80'h00000000000000000000;
