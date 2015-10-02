bb49d303657d35dd817ccfa5319a5aead88fec74
w40141 <w.40141@gmail.com>
2015-10-02 10:42:30 +0900

    gg

--- Trivium/kairo/e_test.v
+++ Trivium/kairo/e_test.v
@@ -15,20 +15,24 @@ assign max = fst + len;
 always @(posedge clk or negedge reset) begin
     if(!reset) begin
         if(count <= max) begin
-            if(count == 0) SET = {3'b111, 112'b0, IV, 13'b0, KEY};
+            if(count == 0) begin
+                SET = {3'b111, 112'b0, IV, 13'b0, KEY};
+            end
             t1 = SET[65]  ^ SET[92];
             t2 = SET[161] ^ SET[176];
             t3 = SET[242] ^ SET[287];
-            if(fst <= count) STRM[len - count + fst - 1] = t1 ^ t2 ^ t3;
-            // if(fst <= count) STRM[count - fst] = t1 ^ t2 ^ t3;
+            if(fst <= count) begin
+                STRM[len - count + fst - 1] = t1 ^ t2 ^ t3;
+                // STRM[count - fst] = t1 ^ t2 ^ t3;
+            end
             t1 = t1 ^ SET[90]  & SET[91]  ^ SET[170];
             t2 = t2 ^ SET[174] & SET[175] ^ SET[263];
             t3 = t3 ^ SET[285] & SET[286] ^ SET[68];
-            SET[92:0]    <= {SET[91:0], t3};
-            SET[176:93]  <= {SET[175:93], t1};
-            SET[287:177] <= {SET[286:177], t2};
+            SET   <= {SET[286:177], t2, SET[175:93], t1, SET[91:0], t3};
             count <= count + 1;
-        end else OUT <= STRM;
+        end else begin
+            OUT <= STRM;
+        end
     end else begin
         count <= 0;
     end
--- Trivium/kairo/test_te.v
+++ Trivium/kairo/test_te.v
@@ -1,17 +1,14 @@
 `timescale 1ns/1ns
 
 module test;
-    reg  [79:0] KEY;
-    reg  [79:0] IV;
+    reg  [79:0] KEY, IV;
     reg [15:0] len;
-    reg clk;
-    reg reset;
+    reg clk, reset;
     wire [4095:0] OUT;
     parameter times = 0.5;
     always #(times)   clk = ~clk;
 
     ENCRIPT ENCRIPT (KEY, IV, len, clk, reset, OUT);
-    // ENCRIPT ENCRIPT (KEY, IV, clk, reset, OUT);
 
     initial begin
         clk = 1'b0;
@@ -19,11 +16,11 @@ module test;
         IV  = 80'h00000000000000000000;
         len = 4096;
         reset = 1;
-        repeat(2) @(negedge clk);
+        repeat(1) @(negedge clk);
         reset = 0;
         while(reset) @(negedge clk);
         @(negedge clk);
-        repeat(20000) @(negedge clk);
+        repeat(15000) @(negedge clk);
         $finish;
     end
 
