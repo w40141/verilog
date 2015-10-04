718519a02fccf948e1f6f4db220e3295ec76448e
w40141 <w.40141@gmail.com>
2015-10-04 23:57:13 +0900

    finisih

--- Trivium/work/e_test.v
+++ Trivium/work/e_test.v
@@ -1,12 +1,15 @@
-module ENCRIPT (KEY, IV, len, clk, reset, OUT);
+module ENCRIPT (KEY, IV, len, clk, reset, OUT_1, OUT_2);
+// module ENCRIPT (KEY, IV, len, clk, reset, OUT);
 input [79:0] KEY, IV;
 input [15:0] len;
 input clk, reset;
-output [511:0] OUT;
+output [4095:0] OUT_1;
+output [4095:0] OUT_2;
 wire [15:0] fst, max;
 reg [15:0] count;
 reg [287:0] SET;
-reg [511:0] STRM, OUT;
+reg [4095:0] STRM_1, OUT_1;
+reg [4095:0] STRM_2, OUT_2;
 reg t1, t2, t3;
 
 assign fst = 4 * 288;
@@ -14,18 +17,29 @@ assign max = fst + len;
 
 always @(posedge clk or negedge reset) begin
     if(!reset) begin
-        if(count < max) begin
-            if(count == 0) SET = {3'b111, 112'b0, IV, 13'b0, KEY};
+        if(count <= max) begin
+            if(count == 0) begin
+                SET = {3'b111, 112'b0, IV, 13'b0, KEY};
+            end
             t1 = SET[65]  ^ SET[92];
             t2 = SET[161] ^ SET[176];
             t3 = SET[242] ^ SET[287];
-            if(fst <= count) STRM[count - fst] = t1 ^ t2 ^ t3;
-            SET[92:0]    = {SET[91:0], t3 ^ SET[285] & SET[286] ^ SET[68]};
-            SET[176:93]  = {SET[175:93], t1 ^ SET[90]  & SET[91]  ^ SET[170]};
-            SET[287:177] = {SET[286:177], t2 ^ SET[174] & SET[175] ^ SET[263]};
+            if(fst <= count) begin
+                STRM_1[count - fst] = t1 ^ t2 ^ t3;
+                STRM_2[len - count + fst - 1] = t1 ^ t2 ^ t3;
+            end
+            t1 = t1 ^ (SET[90]  & SET[91] ) ^ SET[170];
+            t2 = t2 ^ (SET[174] & SET[175]) ^ SET[263];
+            t3 = t3 ^ (SET[285] & SET[286]) ^ SET[68];
+            SET   <= {SET[286:177], t2, SET[175:93], t1, SET[91:0], t3};
             count <= count + 1;
-        end else OUT <= STRM;
-    end else count <= 0;
+        end else begin
+            OUT_1 <= STRM_1;
+            OUT_2 <= STRM_2;
+        end
+    end else begin
+        count <= 0;
+    end
 end
 
 endmodule
--- Trivium/work/test_te.v
+++ Trivium/work/test_te.v
@@ -1,35 +1,35 @@
 `timescale 1ns/1ns
 
 module test;
-    reg  [79:0] KEY;
-    reg  [79:0] IV;
+    reg  [79:0] KEY, IV;
     reg [15:0] len;
-    reg clk;
-    reg reset;
-    wire [511:0] OUT;
+    reg clk, reset;
+    wire [4095:0] OUT_1;
+    wire [4095:0] OUT_2;
     parameter times = 0.5;
     always #(times)   clk = ~clk;
 
-    ENCRIPT ENCRIPT (KEY, IV, len, clk, reset, OUT);
-    // ENCRIPT ENCRIPT (KEY, IV, clk, reset, OUT);
+    ENCRIPT ENCRIPT (KEY, IV, len, clk, reset, OUT_1, OUT_2);
+    // ENCRIPT ENCRIPT (KEY, IV, len, clk, reset, OUT);
 
     initial begin
-        clk = 0;
-        KEY = 80'h10000000000000000000;
+        clk = 1'b0;
+        // KEY = 80'hFF000102030405060708;
+        KEY = 80'h080706050403020100FF;
         IV  = 80'h00000000000000000000;
-        // len = 512 * 8;
-        len = 512;
+        len = 4096;
         reset = 1;
-        repeat(2) @(negedge clk);
+        repeat(1) @(negedge clk);
         reset = 0;
         while(reset) @(negedge clk);
         @(negedge clk);
-        repeat(2000) @(negedge clk);
+        repeat(15000) @(negedge clk);
         $finish;
     end
 
     initial begin
-        $monitor($time, ", KEY=%h, IV=%h, OUT=%h", KEY, IV, OUT);
+        $monitor($time, ", KEY=%h, IV=%h\n\nOUT_1=%h\n\nOUT_2=%h", KEY, IV, OUT_1, OUT_2);
+        // $monitor($time, ", KEY=%h, IV=%h, OUT=%h", KEY, IV, OUT_1, OUT_2);
         $dumpfile("trivium.vcd");
         $dumpvars(0, test);
     end
