04e97411e74116a9701c34efcfb0ffc6f2a120f6
w40141 <w.40141@gmail.com>
2015-11-20 18:14:42 +0900

    make trivium

--- Trivium/analysis/trivium.py
+++ Trivium/analysis/trivium.py
@@ -1,11 +1,8 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-# ININUM = 288 * 4
-ININUM = 288
-# STREAM = 128
-STREAM = 0
-NUMSTR = ININUM + STREAM
+CYCLES = 100
+INREGS = 288
 
 
 def inputHex(rawData):
@@ -36,25 +33,30 @@ def shiftFunc(reg):
 
 if __name__ == "__main__":
     inReg = []
+    tmp = ''
     print("input key")
     # raw_key = raw_input()
-    raw_key = '00000000000000000000'
+    raw_key = '11111111111111111111'
     key = inputHex(raw_key)
     print('input inputfile name')
     # inputFile = raw_input()
     inputFile = 'randomNum.txt'
     print('input outputfile name')
     outputFile = raw_input()
+    signe = [['' for i in range(CYCLES)] for j in range(INREGS)]
     with open(inputFile, "r") as fi:
         fl = fi.readlines()
         for i in fl:
             iv = inputHex(i[:-1].replace(' ', ''))
             reg = initfunc(key, iv)
-            for j in range(NUMSTR + 1):
+            for j in range(CYCLES):
                 reg = shiftFunc(reg)
                 strReg = ''.join(map(str, reg))
                 inReg.append(strReg)
                 with open(outputFile, 'a') as fh:
                     fh.write(strReg + '\n')
-            print(inReg[0][288])
-            print('Finish' + str(i))
+    for i in range(INREGS):
+        for cyc in range(CYCLES):
+            for j in range(len(fl)):
+                signe[i][cyc] = signe[i][cyc] + inReg[cyc + CYCLES * j][i]
+    print(signe)
