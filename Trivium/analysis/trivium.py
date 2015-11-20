2ba27571b7cb83f8bec543a3a155005745104ef6
w40141 <w.40141@gmail.com>
2015-11-20 13:24:18 +0900

    make randomNum

--- Trivium/analysis/trivium.py
+++ Trivium/analysis/trivium.py
@@ -1,7 +1,11 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-ININUM = 288 * 4
+# ININUM = 288 * 4
+ININUM = 288
+# STREAM = 128
+STREAM = 0
+NUMSTR = ININUM + STREAM
 
 
 def inputHex(rawData):
@@ -30,54 +34,27 @@ def shiftFunc(reg):
     return reg
 
 
-def firstFunc(reg):
-    t1 = reg[65] ^ reg[92]
-    t2 = reg[161] ^ reg[176]
-    t3 = reg[242] ^ reg[287]
-    return t1, t2, t3
-
-
-def secondFunc(reg, t1, t2, t3):
-    t1 = t1 ^ reg[90] & reg[91] ^ reg[170]
-    t2 = t2 ^ reg[174] & reg[175] ^ reg[263]
-    t3 = t3 ^ reg[285] & reg[286] ^ reg[68]
-    reg[1:] = reg[:287]
-    reg[0] = t3
-    reg[93] = t1
-    reg[177] = t2
-    return reg
-
-
-def makeZ(z, reg):
-    t1, t2, t3 = firstFunc(reg)
-    if i > ININUM:
-        z.append(t1 ^ t2 ^ t3)
-    reg = secondFunc(reg, t1, t2, t3)
-    return z, reg
-
-
 if __name__ == "__main__":
-    z = []
-    stream = 128
+    inReg = []
     print("input key")
     # raw_key = raw_input()
     raw_key = '00000000000000000000'
     key = inputHex(raw_key)
     print('input inputfile name')
     # inputFile = raw_input()
-    inputFile = 'test.txt'
+    inputFile = 'randomNum.txt'
     print('input outputfile name')
     outputFile = raw_input()
     with open(inputFile, "r") as fi:
         fl = fi.readlines()
-        print(fl)
         for i in fl:
             iv = inputHex(i[:-1].replace(' ', ''))
             reg = initfunc(key, iv)
-            for j in range(stream + ININUM + 1):
+            for j in range(NUMSTR + 1):
                 reg = shiftFunc(reg)
-                # z, reg = makeZ(z, reg)
                 strReg = ''.join(map(str, reg))
-                print(strReg)
+                inReg.append(strReg)
                 with open(outputFile, 'a') as fh:
                     fh.write(strReg + '\n')
+            print(inReg[0][288])
+            print('Finish' + str(i))
