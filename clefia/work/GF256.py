d4038d7999e6ee80ae074fae895056a420eddbcd
w40141 <w.40141@gmail.com>
2017-11-28 17:45:11 +0900

    aa

--- clefia/work/GF256.py
+++ clefia/work/GF256.py
@@ -1,9 +1,10 @@
 import numpy as np
 
+
 class GF256:
 
-    zero=0x11D
-    #x^8+x^4+x^3+x^2+1==0
+    zero = 0x11D
+    # x^8+x^4+x^3+x^2+1==0
 
     def __init__(self, n):
         self.n = n
@@ -42,7 +43,7 @@ class GF256:
     def inv(self):
         GF256.init()
         if self.n == 0:
-            return GF256(0) #error
+            return GF256(0)  # error
         exponent_0 = GF256.v2e_table[self.n]
         exponent_1 = 255 - exponent_0
         r = GF256(GF256.e2v_table[exponent_1])
@@ -50,28 +51,30 @@ class GF256:
 
     @classmethod
     def init(cls):
-        if hasattr(cls, 'v2e_table') :return
+        if hasattr(cls, 'v2e_table'):
+            return
         cls.v2e_table = np.zeros(256, dtype='uint16')
         cls.e2v_table = np.zeros(256, dtype='uint8')
         node = GF256(1)
         for i in range(255):
-            #print("(%d,%d)" % (i,node.n))
-            cls.e2v_table[i]=node.n
-            cls.v2e_table[node.n]=i
+            # print("(%d,%d)" % (i,node.n))
+            cls.e2v_table[i] = node.n
+            cls.v2e_table[node.n] = i
             node = node.shift()
 
+
 if __name__ == "__main__":
-  a = GF256(0xA0).inv()
-  a.print()
-  b = GF256(0x00).mul(a)
-  b.print()
-  c = GF256(0x5D).mul(a)
-  c.print()
-  d = GF256(0xAD).mul(a)
-  d.print()
-  e = GF256(0xA0).mul(a)
-  e.print()
-  f = GF256(0xF0).mul(a)
-  f.print()
-  g = GF256(0x70).mul(a)
-  g.print()
+    a = GF256(0xA0).inv()
+    a.print()
+    b = GF256(0x00).mul(a)
+    b.print()
+    c = GF256(0x5D).mul(a)
+    c.print()
+    d = GF256(0xAD).mul(a)
+    d.print()
+    e = GF256(0xA0).mul(a)
+    e.print()
+    f = GF256(0xF0).mul(a)
+    f.print()
+    g = GF256(0x70).mul(a)
+    g.print()
