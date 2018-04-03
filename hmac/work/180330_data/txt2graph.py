4f06617be326cfe679577fb264021df6b67305cb
w40141 <w.40141@gmail.com>
2018-04-03 16:59:29 +0900

    aa

--- hmac/work/180330_data/txt2graph.py
+++ hmac/work/180330_data/txt2graph.py
@@ -17,6 +17,7 @@ def file2data(fn):
 def make_summary(t_li):
     t = np.array(t_li)
     n_max = np.max(t)
+    print(n_max)
     n_min = np.min(t)
     n_ave = np.mean(t)
     n_std = np.std(t, ddof=1)
@@ -49,7 +50,8 @@ def make_plot(x, summary_li, mode):
     plt.errorbar(x, s_ave, yerr=s_std, fmt='b.', ecolor='g', capsize=3.0)
     plt.xlabel("Bit length of the scan data [bits]")
     plt.ylabel(yl)
-    plt.legend()
+    plt.legend(bbox_to_anchor=(0.5, -0.2), loc='center', borderaxespad=0, ncol=4)
+    plt.subplots_adjust(bottom=0.2)
     plt.show()
 
 
@@ -58,8 +60,8 @@ def main():
     file_li = os.listdir(dir_path)
     time_x, time_summary_li = file2summary(dir_path, file_li, 'time')
     make_plot(time_x, time_summary_li, 'time')
-    # word_x, word_summary_li = file2summary(dir_path, file_li, 'word')
-    # make_plot(word_x, word_summary_li, 'word')
+    word_x, word_summary_li = file2summary(dir_path, file_li, 'word')
+    make_plot(word_x, word_summary_li, 'word')
 
 
 if __name__ == "__main__":
